e6ee993decfadfbc15f1886544b7ab6c0c91c3ff
tatsuya azegami <theorthodoxworks@gmail.com>
2017-04-28 00:00:17 +0900

    Add async svg sprite

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,10 @@
 // ----------------------
 
 import Test from './modules/Test';
+import asyncSvgSprite from './modules/asyncSvgSprite';
 
 const target = document.getElementById('js-test');
 const test = new Test(target, 'Hello, webpack.');
 test.hello();
+
+asyncSvgSprite();
